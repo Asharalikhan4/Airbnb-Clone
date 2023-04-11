93d76fcaba5df51ccca5744bf8cb2c367e397920
Ashar Ali Khan <asharalikhan@Ashars-MacBook-Air.local>
2023-04-12 00:37:15 +0530

    So far everything working fine check profile route

---
+++ frontend/src/App.jsx
@@ -0,0 +1,26 @@
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import axios from "axios";
+import IndexPage from "./pages/IndexPage";
+import LoginPage from "./pages/LoginPage";
+import Layout from "./Layout";
+import RegisterPage from "./pages/RegisterPage";
+import { UserContextProvider } from "./UserContext";
+
+axios.defaults.baseURL = "http://localhost:8080";
+
+function App() {
+  return (
+    <UserContextProvider>
+      <Routes>
+      <Route path="/" element={<Layout />}>
+      <Route path="/" element={<IndexPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+      </Route>
+    </Routes>
+    </UserContextProvider>
+  )
+}
+
+export default App;
